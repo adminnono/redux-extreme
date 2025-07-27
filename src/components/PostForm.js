50b302928e6824c10719495df787145cc8c3ddcc
arnaud <arnaud.dujardin76@gmail.com>
2025-07-27 12:11:21 +0200

    Ajout données db.json côté Frontend depuis le store

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useSelector } from "react-redux";
 
 const PostForm = () => {
+  const form = useRef();
+  const user = useSelector((state) => state.userReducer);
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+  };
+
+  const postData = {
+    author: user.pseudo,
+    title: form.current[0].value,
+    content: form.current[1].value,
+    likes: 0,
+  };
+
   return (
     <div className="form-container">
-      <form>
+      <form ref={form} onSubmit={(e) => handleForm(e)}>
         <input type="text" placeholder="Titre du poste" />
         <textarea placeholder="Postez vos pensées..."></textarea>
         <input type="submit" value="Envoyer" />
